b3d4ebca76ab7708fb822992f121d4e987fd852b
“[Nurjana <[nurjana.a@gmail.com]>
2020-05-25 03:00:42 -0500

    hi

--- src/OfficeHours2/day4/xyzMiddle.java
+++ src/OfficeHours2/day4/xyzMiddle.java
@@ -1,4 +1,24 @@
 package OfficeHours2.day4;
 
 public class xyzMiddle {
+    public static void main(String[] args) {
+        String s = "java is java is java";
+
+        System.out.println(s.indexOf("java", s.length()/2 - 4));
+
+    }
+
+    public boolean xyzMiddle(String str) {
+
+        if(str.length() < 3) return false;
+
+        int mid = str.indexOf("xyz", str.length()/2 - 3);
+
+        int before = str.substring(0,mid).length();
+        int after = str.substring(mid + 3).length();
+
+        return before == after || before + 1 == after || before == after + 1;
+
+    }
+
 }
--- src/Replit/Value.java
+++ src/Replit/Value.java
@@ -5,19 +5,21 @@ public class Value {
     private boolean wasModified = false;
     //WRITE YOUR CODE HERE
     //a constructor accepting a single integer parameter
-    public Value(int a){
-        this.val=val;
+    public Value(int val) {this.val = val;}
+   //a constructor with no parameters
+    public Value() { }
+   // method 'setVal' that accepts a single parameter,
+    public void setVal(int val) {this.val = val; wasModified = true;}
+    //a boolean method, 'wasModified' that returns true if setVal was ever called for the object.
+    public boolean wasModified() {return wasModified;}
+//    a method 'getVal' that returns an integer value as follows: if setVal has ever been called,
+//    it getVal returns the last value passed to setVal. Otherwise if the "single int parameter"
+//    constructor was used to create the object, getVal returns the value passed to that  constructor.
+//    Otherwise getVal returns 0.
+
+    public int getVal() {return val;}
     }
-    //a constructor with no parameters
-    public Value(){}
-   // a method 'setVal' that accepts a single parameter,
-    public void setVal(int b){
-    }
-   //a boolean method, 'wasModified' that returns true if setVal was ever called for the object.
-    public boolean wasModified(){
-        return true;
-    }
-}
+
 
 
 /*
